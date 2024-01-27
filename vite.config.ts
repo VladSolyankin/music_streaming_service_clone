9f38352055878bc1d8076a76738ddd82651afb36
VladSolyankin <darnell.dewberry@mail.ru>
2024-01-27 12:47:11 +0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,28 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+
+        assetFileNames: ({name}) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')){
+            return 'assets/[name]-[hash][extname]';
+          }
+
+          if (/\.css$/.test(name ?? '')) {
+            return 'css/[name]-[hash][extname]';
+          }
+
+          // default value
+          // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+          return '[name]-[hash][extname]';
+        },
+      },
+    }
+  },
   server: {
     proxy: {
       "/getArtists": {
